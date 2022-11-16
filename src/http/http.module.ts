0c7ce4f08dfc207e25e4a96d9beb6e4a16ae6da6
salesof7 <jpsaless2002@gmail.com>
2022-11-16 18:18:29 -0300

    feat: add graphql and import it as module in http module

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from 'src/database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import path from 'node:path';
+
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
 })
 export class HttpModule {}
