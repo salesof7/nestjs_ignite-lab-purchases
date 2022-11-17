2bd2fea6ecf53fb720a5c49e736ba0593d02ed8f
salesof7 <jpsaless2002@gmail.com>
2022-11-17 20:14:00 -0300

    feat: compose relationship between purchases and products with graphql's ResolveField

--- src/http/graphql/resolvers/purchases.resolver.ts
+++ src/http/graphql/resolvers/purchases.resolver.ts
@@ -1,16 +1,25 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { ProductsService } from '../../../services/products.service';
 import { PurchasesService } from '../../../services/purchases.service';
 import { AuthorizationGuard } from '../../auth/authorization.guard';
 import { Purchase } from '../models/purchase';
 
-@Resolver()
+@Resolver(() => Purchase)
 export class PurchasesResolver {
-  constructor(private purchasesService: PurchasesService) {}
+  constructor(
+    private purchasesService: PurchasesService,
+    private productsService: ProductsService,
+  ) {}
 
   @Query(() => [Purchase])
   @UseGuards(AuthorizationGuard)
   purchases() {
     return this.purchasesService.listAllPurchases();
+  }
+
+  @ResolveField()
+  product(@Parent() purchase: Purchase) {
+    return this.productsService.getProductById(purchase.productId);
   }
 }
