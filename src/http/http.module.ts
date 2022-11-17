eaee852fcb76040e58df0e4333f2432e57980b66
salesof7 <jpsaless2002@gmail.com>
2022-11-17 02:59:35 -0300

    feat: create product model and resolver for graphql

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,9 +1,11 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 
 import { DatabaseModule } from '../database/database.module';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
 
 @Module({
   imports: [
@@ -11,7 +13,9 @@ import { DatabaseModule } from '../database/database.module';
     DatabaseModule,
     GraphQLModule.forRoot({
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
     }),
   ],
+  providers: [ProductsResolver],
 })
 export class HttpModule {}
